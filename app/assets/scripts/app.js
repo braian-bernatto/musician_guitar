64e85632eaa29c37580062e7a54e316a0c2802e0
Bernatto <braianbernatto@hotmail.com>
2021-07-18 14:54:50 -0400

    cleaning code

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,10 +1,8 @@
 import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
-import Contact from "./modules/Contact"
 import Footer from "./modules/Footer"
 
 let mobileMenu = new MobileMenu()
-let contact = new Contact()
 let footer = new Footer()
 
 if (module.hot) {
